dbda016e2e7923256116797a2c46e1bae793a545
hasnat.jamil <hasnat.jamil@enosisbd.com>
2024-05-21 01:40:31 +0600

    changeset: add db method signatures for template

---
+++ src/db/template.ts
@@ -0,0 +1,21 @@
+import { Template } from "@/models/Template";
+
+export const addTemplate = async (template: Template) => {
+  //add the template in the database
+};
+
+export const getTemplateById = async (id: string) => {
+  //get the template from the database by id
+};
+
+export const getAllTemplates = async () => {
+  //get all the templates from the database
+};
+
+export const updateTemplate = async (template: Template) => {
+  //update the template in the database
+};
+
+export const deleteTemplate = async (id: string) => {
+  //delete the template from the database
+};
