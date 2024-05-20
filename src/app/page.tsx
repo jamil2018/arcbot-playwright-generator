52e489cffd586951ba655a19afacec2ad0b7cc9e
hasnat.jamil <hasnat.jamil@enosisbd.com>
2024-05-20 17:18:14 +0600

    changeset: add UI library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main>
       <h1>Home</h1>
       <p>Welcome to the Arcbot Playwright Generator</p>
+      <Button variant="outline">hello world</Button>
     </main>
   );
 }
